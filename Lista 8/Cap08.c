87159d59b8cc936496542cd197e5fa382c235326
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-06-08 23:26:28 -0300

    1° exercício terminado

--- Lista 8/Cap08.c
+++ Lista 8/Cap08.c
@@ -1,23 +1,7 @@
-#define ex01
+#define ex02
 
 #ifdef ex01
 #include <stdio.h>
-
-   /* struct dados
-    {
-        char _char;
-        int _int;
-        long int _long;
-        float _float;
-        double _double;
-        unsigned char _uchar;
-        unsigned int _uint;
-        unsigned long _ulong;
-    };
-
-    struct dados estrutura;
-
-    */
 main()
 {
     char continuar = 'a', *pchar, _char;
@@ -26,12 +10,15 @@ main()
     long *plong, _long;
     unsigned int *puint, _uint;
     unsigned long *pulong, _ulong;
-    float *pfloat, _float;
-    double *pdouble, _double;
+    float _float, *pfloat;
+    double _double, *ponteiro_double;
 
+    ponteiro_double = &_double;
+    pfloat = &_float;
     pchar = &_char;
-    puchar = &_uchar; pint = &_int; plong = &_long; puint = &_uint;
-    pulong = &_ulong; pfloat = &_float; pdouble = &_double;
+    puchar = &_uchar;
+    pint = &_int; plong = &_long; puint = &_uint;
+    pulong = &_ulong;
 
     do{
 
@@ -48,29 +35,27 @@ main()
     scanf("%f",&_float);
 
     printf("\nDigite um valor double: ");
-    scanf("%lf",&_double);  printf("%Double = %lf",_double);
+    scanf("%lf",&_double);
 
     printf("\nDigite um valor unsigned char: ");
-    scanf("%u",&_uchar);    printf("%Double = %lf",_double);
+    scanf("%u",&_uchar);
 
     printf("\nDigite um valor unsigned int: ");
-    scanf("%u",&_uint); printf("%Double = %lf",_double);
+    scanf("%u",&_uint);
 
     printf("\nDigite um valor unsigned long: ");
-    scanf("%lu",&_ulong);   printf("%Double = %lf",_double);
+    scanf("%lu",&_ulong);
 
     printf("\nDigite um valor character: ");
-    getchar(); _char = getchar();   printf("%Double = %lf",_double);
+    getchar(); _char = getchar();
 
-    //printf("\n%c, %d, %ld, %f, %lf \n%c, %u, %lu,\n\n",_char, _int, _long, _float, _double, _uchar, _uint, _ulong);
-
-    printf("  ");
+    printf("\n");
     for(cont=10;cont<70;cont+=10)
     {
         printf("%10d",cont);
     }
 
-    printf("\n\n  ");
+    printf("\n  ");
     for(cont=1, rodadas=0;rodadas<6;cont++)
     {
         printf("%d",cont);
@@ -85,12 +70,13 @@ main()
     printf("%-20ld",*plong);
     printf("%-20u",*puint);
 
-    /*printf("%-20f",*pfloat);
-    printf("%-20lf",*pdouble);*/
-    printf("\n            "); printf("AQUIII..."); printf("%lf",*pdouble);
+    printf("\n\t\t");
 
-    printf("%-20lf",*pdouble);
-    printf("%-20f",*pfloat);
+    printf("%-20.2lf",*pfloat);
+    printf("%.2f",*ponteiro_double);
+
+    printf("\n\t   %-20lu",*plong);
+    printf("%c",*puchar);
 
     printf("\n\nDeseja continuar? Digite 's' para 'sim' ou qualquer outra tecla para 'nao': "); getchar();
     continuar = getchar(); /*putchar(continuar);*/
@@ -98,3 +84,21 @@ main()
     }while(continuar == 's' || continuar == 'S');
 }
 #endif //ex01
+
+#ifdef ex02
+
+#include <stdio.h>
+main()
+{
+
+
+
+
+
+
+
+
+
+}
+
+#endif // ex02
