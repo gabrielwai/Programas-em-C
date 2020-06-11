8d217d0110008719761bf43396c837fc67cd5e98
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-06-11 00:26:44 -0300

    Update Cap08.c

--- Lista 8/Cap08.c
+++ Lista 8/Cap08.c
@@ -90,11 +90,115 @@ main()
 #include <stdio.h>
 main()
 {
+    struct dados
+    {
+        char nome[50], endereco[50], cidade[25], cep[9], estado[3];
+    };
+    struct dados cadastro[4];
+
+    int i=0, cont, verificador;
+
+    for(cont=0;cont<4;cont++)
+    {
+        cadastro[cont].cep[8] = 'A';
+        cadastro[cont].estado[2] = 'A';
+    }
+
+    do{
+        verificador = 0;
+        printf("Digite seu nome: ");
+        gets(cadastro[i].nome);
+
+        for(cont=0;cont<50;cont++)
+        {
+            if(cadastro[i].nome[cont] == '\0')
+            {
+                verificador = 1;
+                break;
+            }
+        }
+        if(verificador == 0)
+            printf("\nMaximo de 49 caracteres. Tente novamente.\n");
+
+    }while(verificador == 0);
+
+    do{
+        verificador = 0;
+        printf("\nDigite seu endereco: ");
+        gets(cadastro[i].endereco);
+
+        for(cont=0;cont<50;cont++)
+        {
+            if(cadastro[i].endereco[cont] == '\0')
+            {
+                verificador = 1;
+                break;
+            }
+        }
+
+        if(verificador == 0)
+            printf("\nMaximo de 49 caracteres. Tente novamente.\n");
+
+    }while(verificador == 0);
+
+    do{
+        verificador = 0;
+        printf("\nDigite sua cidade: ");
+        gets(cadastro[i].cidade);
+
+        for(cont=0;cont<25;cont++)
+        {
+            if(cadastro[i].cidade[cont] == '\0')
+            {
+                verificador = 1;
+                cont = 25;
+            }
+        }
+
+        if(verificador == 0)
+            printf("\nMaximo de 24 caracteres. Tente novamente.\n");
+
+    }while(verificador == 0);
+
+    do{
+        verificador = 0;
+        printf("\nDigite seu cep: ");
+        scanf("%s",&cadastro[i].cep);
+
+        if(cadastro[i].cep[8] == '\0')
+        {
+            verificador = 1;
+        }
+
+        if(verificador == 0)
+            printf("\nDigite apenas 8 caracteres. Tente novamente.\n");
+
+    }while(verificador == 0);
 
 
+    do{
+        verificador = 0;
+        printf("\nDigite seu estado (no formato XX): ");
+        scanf("%s",&cadastro[i].estado);
 
+        if(cadastro[i].estado[2] == '\0')
+        {
+            verificador = 1;
+        }
 
+        if(verificador == 0)
+            printf("\nMaximo de 2 caracteres. Tente novamente.\n");
 
+    }while(verificador == 0);
+
+    for(i=0;i<4;i++){
+    printf("\nCadastro foi: %s\n",cadastro[i].nome);
+    printf("\nCadastro foi: %s\n",cadastro[i].endereco);
+    printf("\nCadastro foi: %s\n",cadastro[i].cidade);
+    printf("\nCadastro foi: %s\n",cadastro[i].cep);
+    printf("\nCadastro foi: %s\n\n",cadastro[i].estado);
+
+    }
 
 
 
